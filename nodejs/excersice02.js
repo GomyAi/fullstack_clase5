872a76ada99a5674a19c08e136841825dc7dfd0c
yesica godinez <yesica.gmata@hotmail.com>
2019-05-24 21:56:48 -0500

    clase 5

---
+++ nodejs/excersice02.js
@@ -0,0 +1,8 @@
+const nivel = 20
+if (nivel <10) {
+            console.log ("Sos Aprendiz")
+        }else if (nivel <=19){
+            console.log ("Sos “Acróbata”")
+        }else{
+            console.log ("Sos ninja")
+        } 
